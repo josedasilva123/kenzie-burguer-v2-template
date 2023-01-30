3feeb57045717ed496785bfc9e892f93750bc95f
Alex Conder <alex.v.conder@gmail.com>
2023-01-30 18:33:19 -0300

    feat: importing global styles and remoing unsed state

--- kenzie-burger-v2/src/App.tsx
+++ kenzie-burger-v2/src/App.tsx
@@ -1,11 +1,10 @@
-import { useState } from 'react'
 import Router from './routes'
+import { GlobalStyles } from './styles/global'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div className="App">
+      <GlobalStyles />
       <Router />
     </div>
   )
