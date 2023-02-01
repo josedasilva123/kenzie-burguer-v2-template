70bb785a0d25276173201f54bf61f6d66bee5868
Alex Conder <alex.v.conder@gmail.com>
2023-02-01 17:44:13 -0300

    feat: creating styled grid box and adding a prop in styled container

--- kenzie-burger-v2/src/styles/grid.ts
+++ kenzie-burger-v2/src/styles/grid.ts
@@ -1,8 +1,19 @@
 import styled from "styled-components";
 
-export const StyledContainer = styled.div`
+interface iStyledContainerProps{
+    containerWidth?: number;
+}
+
+export const StyledContainer = styled.div<iStyledContainerProps>`
     width: 100%;
-    max-width: 1000px;
+    max-width: ${({containerWidth}) => containerWidth ? containerWidth : 1000}px;
     margin: 0 auto;
     padding: 10px;
+`
+
+export const StyledGridBox = styled.div`
+    padding: 20px;
+    background: ${({theme}) => theme.colors.white};
+    border: 2px solid ${({theme}) => theme.colors.gray0};
+    border-radius: 5px;
 `
