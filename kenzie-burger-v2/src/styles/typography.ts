d3a075065ed2ffd5cc68dfacd2b75b38f14db52e
Alex Conder <alex.v.conder@gmail.com>
2023-02-01 17:43:49 -0300

    feat add some options to paragraph (color and textalign)

--- kenzie-burger-v2/src/styles/typography.ts
+++ kenzie-burger-v2/src/styles/typography.ts
@@ -1,57 +1,82 @@
-import React from 'react';
+import React from "react";
 import styled, { css, isStyledComponent } from "styled-components";
-import BaseTitle from './components/BaseTitle';
+import BaseTitle from "./components/BaseTitle";
 
-interface iStyledTitleProps{
-    $fontSize: "one" | "two" | "three" | "four";
-} 
+interface iStyledTitleProps {
+   $fontSize: "one" | "two" | "three" | "four";
+}
+
+interface iStyledParagraphProps {
+   fontColor?: "gray" | "red";
+   textAlign?: "center" | "left" | "right";
+}
 
 export const StyledTitle = styled(BaseTitle)<iStyledTitleProps>`
-    font-family: ${({theme}) => theme.fonts.primary};
-    line-height: 1.6;
+   width: 100%;
 
-    ${({$fontSize}) => {
-        switch($fontSize){
-            case 'one':
-                return css`
-                    font-size: 1.55rem;
-                    font-weight: 700;
-                `
-            case 'two':
-                return css`
-                    font-size: 1.35rem;
-                    font-weight: 700;
-                `    
-            
-            case 'three':
-                return css`
-                    font-size: 1.15rem;
-                    font-weight: 700;
-                `
+   font-family: ${({ theme }) => theme.fonts.primary};
+   line-height: 1.6;
 
-            case 'four':
-                return css`
-                    font-size: 1rem;
-                    font-weight: 400;
-                `   
+   ${({ $fontSize }) => {
+      switch ($fontSize) {
+         case "one":
+            return css`
+               font-size: 1.55rem;
+               font-weight: 700;
+            `;
+         case "two":
+            return css`
+               font-size: 1.35rem;
+               font-weight: 700;
+            `;
 
-        }
-    }}
-`
+         case "three":
+            return css`
+               font-size: 1.15rem;
+               font-weight: 700;
+            `;
 
-export const StyledParagraph = styled.p`
-    font-family: ${({theme}) => theme.fonts.primary};
-    font-size: .875rem;
-    font-weight: 400;
-    line-height: 1.4;
-    
-    strong{
-        font-weight: 600;
-    }
-`
+         case "four":
+            return css`
+               font-size: 1rem;
+               font-weight: 400;
+            `;
+      }
+   }}
+`;
+
+export const StyledParagraph = styled.p<iStyledParagraphProps>`
+   font-family: ${({ theme }) => theme.fonts.primary};
+   font-size: 0.875rem;
+   font-weight: 400;
+   line-height: 1.8;
+
+   text-align: ${({ textAlign }) => textAlign};
+
+   ${({ fontColor, theme }) => {
+      switch (fontColor) {
+         case "gray":
+            return css`
+               color: ${theme.colors.gray300};
+            `;
+         case "red":
+            return css`
+               color: ${theme.colors.feedback.negative};
+            `;
+         default:
+            return css`
+               color: ${theme.colors.gray600};
+            `;
+      }
+   }}
+
+   strong {
+      font-weight: 600;
+   }
+`;
 
 export const StyledCaption = styled.span`
-    font-family: ${({theme}) => theme.fonts.primary};
-    font-size: .75rem;
-    font-weight: 400;
-`
+   font-family: ${({ theme }) => theme.fonts.primary};
+   font-size: 0.75rem;
+   font-weight: 400;
+`;
